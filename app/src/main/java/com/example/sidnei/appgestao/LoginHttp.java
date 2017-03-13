44739565e43cb16d026968728a4df5da1f903ab6
sidnei <sid-bandeira@hotmail.com>
2017-03-13 01:03:43 -0300

    Criado classe http para tela de login

---
+++ app/src/main/java/com/example/sidnei/appgestao/LoginHttp.java
@@ -0,0 +1,33 @@
+package com.example.sidnei.appgestao;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LoginHttp {
+    public static final String urlArquivo = "http://sgestao.hol.es/ws_login.php";
+
+    public static HttpURLConnection connectar() throws IOException {
+        final int SEGUNDOS = 1000;
+        URL url = new URL(urlArquivo);
+        HttpURLConnection conexao = (HttpURLConnection)url.openConnection();
+        conexao.setReadTimeout(10 * SEGUNDOS);
+        conexao.setConnectTimeout(15 * SEGUNDOS);
+        conexao.setRequestMethod("GET");
+        conexao.setDoInput(true);
+        conexao.setDoOutput(false);
+        conexao.connect();
+        return conexao;
+    }
+
+    public static boolean temConexao(Context ctx) {
+        ConnectivityManager cm = (ConnectivityManager)
+                ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return (info != null && info.isConnected());
+    }
+}
