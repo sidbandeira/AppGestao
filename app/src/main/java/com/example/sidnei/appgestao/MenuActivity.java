9edf595f3340d6e9bddeb4b8430f285b6e3854ca
sidnei <sid-bandeira@hotmail.com>
2017-03-22 00:03:32 -0300

    Desenvolvido tela de menus e tratado conforme liberação de cada usuário.

--- app/src/main/java/com/example/sidnei/appgestao/MenuActivity.java
+++ app/src/main/java/com/example/sidnei/appgestao/MenuActivity.java
@@ -1,16 +1,36 @@
 package com.example.sidnei.appgestao;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button btnVendas;
+    private Button btnCaixa;
+    private Button btnProdutos;
+    private Button btnPostos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnCaixa = (Button) findViewById(R.id.btnCaixa);
+        btnVendas = (Button) findViewById(R.id.btnVendas);
+        btnProdutos = (Button) findViewById(R.id.btnProdutos);
+        btnPostos = (Button) findViewById(R.id.btnPostos);
+
+        if(MainActivity.menuLoja ==  0){
+            btnVendas.setVisibility(View.INVISIBLE);
+            btnCaixa.setVisibility(View.INVISIBLE);
+            btnProdutos.setVisibility(View.INVISIBLE);
+        }
+
+        if(MainActivity.menuPosto == 0){
+            btnPostos.setVisibility(View.INVISIBLE);
+        }
     }
 
     // CHAMA A TELA DE RESUMO DA VENDAS DO DIA
