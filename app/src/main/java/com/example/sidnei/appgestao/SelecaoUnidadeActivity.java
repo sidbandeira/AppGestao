2731e2be26486023e26f97fd957f8f9b2f54d0ad
sidnei <sid-bandeira@hotmail.com>
2017-03-22 20:29:38 -0300

    removido variaveis nao utilizadas

--- app/src/main/java/com/example/sidnei/appgestao/SelecaoUnidadeActivity.java
+++ app/src/main/java/com/example/sidnei/appgestao/SelecaoUnidadeActivity.java
@@ -4,14 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SelecaoUnidadeActivity extends AppCompatActivity {
-    public String menuPosto = "";
-    public String menuLoja = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Intent x = getIntent();
-//        menuPosto = x.getStringExtra("menuPosto");
-//        menuLoja = x.getStringExtra("menuLoja");
 
         setContentView(R.layout.activity_selecao_unidade);
     }
