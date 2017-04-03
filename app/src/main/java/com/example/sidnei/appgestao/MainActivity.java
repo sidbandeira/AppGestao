3bd2e1aae2647a6f59d834d975ad43999817db98
sidnei <sid-bandeira@hotmail.com>
2017-04-03 19:31:34 -0300

    corrigido nome do botão

--- app/src/main/java/com/example/sidnei/appgestao/MainActivity.java
+++ app/src/main/java/com/example/sidnei/appgestao/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         edtUsuario = (EditText) findViewById(R.id.edtUsuario);
