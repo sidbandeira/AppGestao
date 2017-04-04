7f19717ebe38a0c0687ff87e00c65cb34b211081
sidnei <sid-bandeira@hotmail.com>
2017-04-04 20:57:36 -0300

    Adicionado primeira versão das classes do pedido de compra

---
+++ app/src/main/java/com/example/sidnei/appgestao/pedidocompra/PedidoCompraActivity.java
@@ -0,0 +1,15 @@
+package com.example.sidnei.appgestao.pedidocompra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sidnei.appgestao.R;
+
+public class PedidoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_compra);
+    }
+}
