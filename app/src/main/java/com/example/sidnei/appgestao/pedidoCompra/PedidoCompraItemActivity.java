9007c78c70225de44cd178c5bd285f83b2ab89df
sidnei <sid-bandeira@hotmail.com>
2017-04-05 19:52:18 -0300

    Activity dos itens do pedido de compra

---
+++ app/src/main/java/com/example/sidnei/appgestao/pedidoCompra/PedidoCompraItemActivity.java
@@ -0,0 +1,15 @@
+package com.example.sidnei.appgestao.pedidoCompra;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sidnei.appgestao.R;
+
+public class PedidoCompraItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_compra_item);
+    }
+}
