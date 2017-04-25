d32d4c93ac30439ea0694a89c46c3a0b6c30e668
sidnei <sid-bandeira@hotmail.com>
2017-04-25 00:49:17 -0300

    Desenvolvimento do pedido de compra, implementação do carregamento do listview

--- app/src/main/java/com/example/sidnei/appgestao/pedidocompra/PedidoCompraItem.java
+++ app/src/main/java/com/example/sidnei/appgestao/pedidocompra/PedidoCompraItem.java
@@ -1,6 +1,7 @@
 package com.example.sidnei.appgestao.pedidoCompra;
 
 public class PedidoCompraItem {
+
     public long _id;
     public long idCompra;
     public long idItem;
@@ -9,6 +10,9 @@ public class PedidoCompraItem {
     public Double precoCusto;
     public Double totalItem;
 
+    public PedidoCompraItem(){
+
+    }
 
     public PedidoCompraItem(long _id, long idCompra, long idItem, String descricaoItem,
                             Double qtdeItem, Double precoCusto, Double totalItem){
@@ -19,6 +23,63 @@ public class PedidoCompraItem {
         this.qtdeItem = qtdeItem;
         this.precoCusto = precoCusto;
         this.totalItem = totalItem;
-
     }
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public long getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(long idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public long getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(long idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getDescricaoItem() {
+        return descricaoItem;
+    }
+
+    public void setDescricaoItem(String descricaoItem) {
+        this.descricaoItem = descricaoItem;
+    }
+
+    public Double getQtdeItem() {
+        return qtdeItem;
+    }
+
+    public void setQtdeItem(Double qtdeItem) {
+        this.qtdeItem = qtdeItem;
+    }
+
+    public Double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(Double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public Double getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(Double totalItem) {
+        this.totalItem = totalItem;
+    }
+
+
 }
