a1c16ca5beca86e565def1b72c86d86710e4ee9b
sidnei <sid-bandeira@hotmail.com>
2017-05-03 22:17:45 -0300

    Implementado persistencia no sqlite do pedido de compra

--- app/src/main/java/com/example/sidnei/appgestao/pedidoCompra/Repositorio/PedidoCompraItemRepositorio.java
+++ app/src/main/java/com/example/sidnei/appgestao/pedidoCompra/Repositorio/PedidoCompraItemRepositorio.java
@@ -5,27 +5,29 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.sidnei.appgestao.pedidoCompra.Classes.PedidoCompraItem;
-import com.example.sidnei.appgestao.pedidoCompra.Persistencia.PedidoCompraItemSQLHelper;
 import com.example.sidnei.appgestao.pedidoCompra.Persistencia.PedidoCompraSQLHelper;
 
 public class PedidoCompraItemRepositorio {
-    private PedidoCompraItemSQLHelper helper;
+    private PedidoCompraSQLHelper helper;
 
+    //CONSTRUTOR
     public PedidoCompraItemRepositorio(Context ctx) {
-        helper = new PedidoCompraItemSQLHelper(ctx);
+        helper = new PedidoCompraSQLHelper(ctx);
     }
 
+    //INSERIR ITENS DO PEDIDO DE COMPRA
     private long inserir(PedidoCompraItem compraItem) {
         SQLiteDatabase db = helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_IDCOMPRA, compraItem.idCompra);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_IDPRODUTO, compraItem.idItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_DESCRICAOPRODUTO, compraItem.descricaoItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_PRECOCUSTO, compraItem.precoCusto);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_QTDEPRODUTO, compraItem.qtdeItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_TOTALPRODUTO, compraItem.totalItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_IDCOMPRA, compraItem.idCompra);
+        cv.put(PedidoCompraSQLHelper.COLUNA_IDPRODUTO, compraItem.idItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_DESCRICAOPRODUTO, compraItem.descricaoItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_PRECOCUSTO, compraItem.precoCusto);
+        cv.put(PedidoCompraSQLHelper.COLUNA_QTDEPRODUTO, compraItem.qtdeItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_TOTALPRODUTO, compraItem.totalItem);
 
-        long id = db.insert(PedidoCompraItemSQLHelper.TABELA, null, cv);
+
+        long id = db.insert(PedidoCompraSQLHelper.TABELAITEM, null, cv);
         if (id != -1) {
             compraItem._id = id;
         }
@@ -33,26 +35,28 @@ public class PedidoCompraItemRepositorio {
         return id;
     }
 
+    //EDITAR ITENS DO PEDIDO DE COMPRA
     private int atualizar(PedidoCompraItem compraItem) {
         SQLiteDatabase db = helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(PedidoCompraSQLHelper.COLUNA_ID, compraItem._id);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_IDCOMPRA, compraItem.idCompra);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_IDPRODUTO, compraItem.idItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_DESCRICAOPRODUTO, compraItem.descricaoItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_PRECOCUSTO, compraItem.precoCusto);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_QTDEPRODUTO, compraItem.qtdeItem);
-        cv.put(PedidoCompraItemSQLHelper.COLUNA_TOTALPRODUTO, compraItem.totalItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_IDPEDIDOITEM, compraItem._id);
+        cv.put(PedidoCompraSQLHelper.COLUNA_IDCOMPRA, compraItem.idCompra);
+        cv.put(PedidoCompraSQLHelper.COLUNA_IDPRODUTO, compraItem.idItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_DESCRICAOPRODUTO, compraItem.descricaoItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_PRECOCUSTO, compraItem.precoCusto);
+        cv.put(PedidoCompraSQLHelper.COLUNA_QTDEPRODUTO, compraItem.qtdeItem);
+        cv.put(PedidoCompraSQLHelper.COLUNA_TOTALPRODUTO, compraItem.totalItem);
 
         int linhasAfetadas = db.update(
-                PedidoCompraItemSQLHelper.TABELA,
+                PedidoCompraSQLHelper.TABELAITEM,
                 cv,
-                PedidoCompraItemSQLHelper.COLUNA_ID +" = ?",
+                PedidoCompraSQLHelper.COLUNA_IDPEDIDOITEM +" = ?",
                 new String[]{ String.valueOf(compraItem._id)});
         db.close();
         return linhasAfetadas;
     }
 
+    //VERIFICAR O TIPO DE GRAVAÇÃO SE EDIÇÃO OU INSERSÃO
     public void salvar(PedidoCompraItem compraItem) {
         if (compraItem._id == 0) {
             inserir(compraItem);
@@ -61,11 +65,12 @@ public class PedidoCompraItemRepositorio {
         }
     }
 
+    //EXCLUIR ITENS DE UM PEDIDO DE COMPRA
     public int excluir(PedidoCompraItem compraItem) {
         SQLiteDatabase db = helper.getWritableDatabase();
         int linhasAfetadas = db.delete(
                 PedidoCompraSQLHelper.TABELA,
-                PedidoCompraSQLHelper.COLUNA_ID +" = ?",
+                PedidoCompraSQLHelper.COLUNA_IDPEDIDOITEM +" = ?",
                 new String[]{ String.valueOf(compraItem._id)});
         db.close();
         return linhasAfetadas;
